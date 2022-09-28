4b74efe1c82b70a3e90143d225d6c2a7b6822bc3
rashmi <>
2022-09-29 00:31:47 +0530

    made changes

---
+++ src/Pages/Customer/Navbar.js
@@ -0,0 +1,37 @@
+import User from "./User";
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faUser, faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import logo from '../../logo.png'; 
+import "./Navbar.css"
+import { Button } from "react-bootstrap";
+
+function Navbar() {
+  return (
+    <div className="navbar-container">
+      <nav className="navbar justify-content-between">
+        <div className="navbar-brand" style={{ height: '3em', marginLeft: "40px" }}>
+        
+        <img src={logo} alt="logo" />
+        <User />
+          
+        </div>
+        <Link
+          to='/cart'
+          className='btn-link'
+        >
+          <div >
+
+            <Button variant="outline-light" style={{ alignItems: "center", borderRadius: '30rem',margin: " 0 40px" }} >
+              <FontAwesomeIcon style={{padding: '0 1em'}}color="#fff" icon={faCartShopping} />
+              Cart
+            </Button>
+          </div>
+        </Link>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
